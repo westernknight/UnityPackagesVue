72187f801763a3d0533321a9493e6caebd92c04e
Your Name <your.email@example.com>
2025-02-01 18:57:15 +0800

    ip

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://192.168.1.6:3000',
         changeOrigin: true
       }
     }
